71e97977816a9ff48acbd76ef02ffa4c85ed2fdd
antonina <antoninasych@gmail.com>
2019-12-01 19:51:19 +0200

    clock

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 
 ReactDOM.render(<App />, document.getElementById('clock'));
-var ghpages = require('gh-pages');
 
-ghpages.publish('dist', function(err) {});
+
 
