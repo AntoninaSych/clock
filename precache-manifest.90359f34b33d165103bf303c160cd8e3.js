c56692d6eae0958aefc6eeda8fe704275f41d65c
antonina <antoninasych@gmail.com>
2019-12-01 20:41:58 +0200

    Updates

---
+++ precache-manifest.90359f34b33d165103bf303c160cd8e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff3ba80096bf531f5c76a7e6f1fe164",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "ee14d2f8e5439f601c62",
+    "url": "/clock/static/css/main.d3eaf03c.chunk.css"
+  },
+  {
+    "revision": "eaf597bbb6c930689a60",
+    "url": "/clock/static/js/2.103e453b.chunk.js"
+  },
+  {
+    "revision": "ee14d2f8e5439f601c62",
+    "url": "/clock/static/js/main.88239c30.chunk.js"
+  },
+  {
+    "revision": "997c12a354189313e8ac",
+    "url": "/clock/static/js/runtime-main.3686412c.js"
+  }
+]);
